9ee4bcfd0a364d34ca67a4600519281cef6ccf0b
Si Cruse <si@sicruse.com>
2024-12-05 10:34:27 -0500

    Add JumpCloud registration

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 # Install language packs for English
-apt install -y $(check-language-support -l en)
+# apt install -y $(check-language-support -l en)
+
+# Register device with JumpCloud
+# curl --tlsv1.2 --silent --show-error --header 'x-connect-key: 2ac1e047c4384b7753273288e0c07bca464b9a57' https://kickstart.jumpcloud.com/Kickstart | sudo bash
+wget --secure-protocol=TLSv1_2 --quiet --header="x-connect-key: 2ac1e047c4384b7753273288e0c07bca464b9a57" -O - https://kickstart.jumpcloud.com/Kickstart | bash
